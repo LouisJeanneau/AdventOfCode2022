f53366f040197d9aad565e798ed50e6d599912e4
Louis Jeanneau <louis.bs.jeanneau@gmail.com>
2022-12-10 16:05:12 +0100

    day 10 me

--- me_day10.py
+++ me_day10.py
@@ -25,14 +25,19 @@ cycle = 0
 
 with open('input.txt') as f:
     for line in f:
+        # noop takes 1 cycle, addx takes 2
         duration = line.count("noop") + 2 * line.count("addx")
         for i in range(duration):
+            # if we begin a new line, we add that line to the screen
             if cycle % 40 == 0:
                 screen.append(["."] * 40)
+            # if x is of value "(cycle%40) +- 1" then the sprite is visible when drawing the current position
             if ((cycle % 40) - 1) <= X <= ((cycle % 40) + 1):
                 screen[cycle // 40][cycle % 40] = "#"
+            # if i == 1, it means we are doing a "addx" operation. i cannot reach 1 when doing "noop"
             if i == 1:
                 X += int(line.split()[1])
+            # We increase cycle at the end, so the drawing phase always occurs for a cycle before the cycle process the instructions
             cycle += 1
 
 for l in screen:
