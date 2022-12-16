fe048600f7afe995cee5eadcbffede3d9e9f839a
Louis Jeanneau <louis.bs.jeanneau@gmail.com>
2022-12-16 03:34:30 +0100

    day 15 me part 2 graphical

--- me_day15.py
+++ me_day15.py
@@ -1,8 +1,15 @@
 import cProfile
 import re
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib.patches import Polygon
+
+mpl.use('Qt5Agg')
+
 
 def manhattan(x1, y1, x2, y2):
     return abs(x1 - x2) + abs(y1 - y2)
+
 
 def part1():
     with open("input.txt") as f:
@@ -19,7 +26,7 @@ def part1():
             beacons.append((beacon_x, beacon_y))
 
         # print(sensors)
-        sensors.sort(key=lambda x:x[0])
+        sensors.sort(key=lambda x: x[0])
         min_x = sensors[0][0] - sensors[0][2]
         max_x = sensors[-1][0] + sensors[-1][2]
         max_dist = max(s[2] for s in sensors)
@@ -29,11 +36,11 @@ def part1():
         y = 2000000
         sum = 0
         iter = max_x + 1 - min_x
-        for x in range(min_x , max_x + 1):
-            if x%100000==0:
+        for x in range(min_x, max_x + 1):
+            if x % 100000 == 0:
                 percent = (x - min_x) / iter * 100
                 print(f'{percent:.2f}%')
-            if (x,y) in beacons:
+            if (x, y) in beacons:
                 continue
             for s in sensors:
                 sx, sy, d = s[0], s[1], s[2]
@@ -43,4 +50,39 @@ def part1():
 
         print(sum)
 
-cProfile.run("part1()")
+
+def part2():
+    with open("input.txt") as f:
+        input = f.read().splitlines()
+        sensors = []
+        beacons = []
+        for line in input:
+            # Extract the integer values from the sentence using the regular expression
+            values_string = re.findall(r'[+-]?\d+', line)
+            sensor_x, sensor_y = int(values_string[0]), int(values_string[1])
+            beacon_x, beacon_y = int(values_string[2]), int(values_string[3])
+            dist = manhattan(sensor_x, sensor_y, beacon_x, beacon_y)
+            sensors.append((sensor_x, sensor_y, dist))
+            beacons.append((beacon_x, beacon_y))
+
+        # print(sensors)
+        sensors.sort(key=lambda x: x[0])
+        min_x = sensors[0][0] - sensors[0][2]
+        max_x = sensors[-1][0] + sensors[-1][2]
+        max_dist = max(s[2] for s in sensors)
+        plt.interactive(True)
+        for sx, sy, d in sensors:
+            d += 1
+            x = (sx, sx + d, sx, sx - d, sx)
+            y = (sy + d, sy, sy - d, sy, sy + d)
+            plt.plot(x,y)
+        plt.axis([0, 4000000, 0, 4000000])
+        # plt.axis('scaled')
+
+        plt.show()
+        plt.pause(1000)
+        return
+
+
+# cProfile.run("part2()")
+part2()
