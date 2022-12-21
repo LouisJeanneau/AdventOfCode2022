6e3f2de6bd3dacaa3b5a22256bf1689ee7574a2f
Louis Jeanneau <louis.bs.jeanneau@gmail.com>
2022-12-21 16:41:01 +0100

    day 21 part 2 smooth

--- Python/day21/me_day21.py
+++ Python/day21/me_day21.py
@@ -1,7 +1,11 @@
+from sympy import symbols, solve
+import sympy as sp
 
 dict = {}
 
+
 def calculus(name: str):
+    # assert name != "humn", "pas bon"
     funct = dict[name]
     if isinstance(funct, int):
         return funct
@@ -11,6 +15,21 @@ def calculus(name: str):
     right = calculus(funct[2])
     ans = eval(str(left) + oper + str(right))
     return ans
+
+
+def print_equation(name: str):
+    if name == "humn":
+        return "x"
+    funct = dict[name]
+    if isinstance(funct, int):
+        return str(funct)
+
+    oper = funct[1]
+    left = print_equation(funct[0])
+    right = print_equation(funct[2])
+    s = "(" + left + oper + right + ")"
+    return s
+
 
 with open("input.txt") as f:
     input = f.read().splitlines()
@@ -22,4 +41,15 @@ with open("input.txt") as f:
             dict[s[0][:-1]] = int(s[1])
 
     # print(dict)
+    # Part 1
     print(calculus("root"))
+
+    # Did some search with assert, the right part of root is non dependant of "humn", so we do the calculus of the right part
+    r = calculus(dict["root"][2])
+    print(r)
+
+    eq = print_equation(dict["root"][0]) + "-" + str(r)
+    print(eq)
+    peq = sp.parse_expr(eq)
+    print(peq)
+    print(int(solve(peq)[0]))
